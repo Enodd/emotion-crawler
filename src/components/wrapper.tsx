13791038dc0ad5b4ebdb22c973d3ffcee0a4e519
Enodd <foxnacity@gmail.com>
2024-04-10 18:47:11 +0200

    changed framework to react

--- src/components/wrapper.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { appWithTranslation } from 'next-i18next';
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const MyApp: React.FC<any> = ({ Component, pageProps }) => (
-	<Component {...pageProps} />
-);
-
-export default appWithTranslation(MyApp);
---
+++ src/providers/Providers.tsx
@@ -0,0 +1,8 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import React, { PropsWithChildren } from 'react';
+
+export const Providers: React.FC<PropsWithChildren> = ({ children }) => {
+    return <ChakraProvider>
+        {children}
+    </ChakraProvider>
+}
